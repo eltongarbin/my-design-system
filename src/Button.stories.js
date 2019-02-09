49dabc1c8d72eac712488252ba34a110c4f619a2
Elton Garbin <eltongarbin@gmail.com>
2019-02-09 21:58:28 -0200

    apply style to a react storybook with the info addon

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
+import { wInfo } from './utils';
 import { Button } from './Button';
 
 storiesOf('Button', module)
-  .addWithJSX('with background', () => (
-    <Button bg="palegoldenrod">Hello world</Button>
-  ))
+  .addWithJSX(
+    'with background',
+    wInfo(`
+      description of the component
+
+      ~~~js
+      <Button>Click here</Button>
+      ~~~
+      `)(() => <Button bg="palegoldenrod">Hello world</Button>)
+  )
   .addWithJSX('with background 2', () => (
     <Button bg="green">Hello world</Button>
   ));
